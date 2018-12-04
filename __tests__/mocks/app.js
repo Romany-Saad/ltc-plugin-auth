f32db3e541db860bcc10e6f3f5a50490c8310a8d
HabibBeshara <habib@cyber-crafts.com>
2018-12-04 14:06:00 +0200

    removed common plugin

--- __tests__/mocks/app.js
+++ __tests__/mocks/app.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const App = require('@lattice/core/lib/App').default
-const CommonPlugin = require('ltc-plugin-common').default
 const ConfigPlugin = require('ltc-plugin-config').default
 const MongoPlugin = require('ltc-plugin-mongo').default
 const MailPlugin = require('ltc-plugin-mail').default
@@ -15,7 +14,6 @@ const app = new App()
 const plugin = new Plugin()
 
 // adding plugins to app instance
-app.addPlugin(new CommonPlugin())
 app.addPlugin(new ConfigPlugin(path.resolve(__dirname, '../config')))
 app.addPlugin(new MailPlugin())
 app.addPlugin(new MongoPlugin())
