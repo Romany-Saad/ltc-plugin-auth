df674f7097aab6ee26ce83966e2cd828c1057f52
HabibBeshara <habib@cyber-crafts.com>
2019-02-14 14:05:11 +0200

    big fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 export const merge = (...items: Array<any>) => {
   let result = {}
   for (let item of items) {
-    result = { ...item }
+    result = { ...result, ...item }
   }
   return result
 }
