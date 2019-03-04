bd8309f8d6b64bf9cd451533c94ff5d242480655
HabibBeshara <habib@cyber-crafts.com>
2019-03-04 11:39:20 +0200

    bug fix

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -25,10 +25,10 @@ export default class Auth {
     const permissionRepo = this.app.get<AMongoDbRepository<any>>(names.AUTH_PERMISSIONS_REPOSITORY)
     const userRepo = this.app.get<AMongoDbRepository<any>>(names.AUTH_USERS_REPOSITORY)
     let user = (await userRepo.findByIds([ this.authorizationData.userId ]))[ 0 ]
-    let permissions = user.data.permissions
+    let permissions = user.data.permissions.map((p: any) => p.name)
     let permissionsNames
     if (permissions.length > 0) {
-      permissions = await permissionRepo.findByIds(permissions)
+      permissions = await permissionRepo.find({ name: { $in: permissions } })
       permissionsNames = permissions.length > 0 ? permissions.map((p: any) => {
         return { name: p.data.name, data: {} }
       }) : []
