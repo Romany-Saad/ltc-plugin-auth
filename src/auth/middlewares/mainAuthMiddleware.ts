4a734ee99138207b67f0220a121edc744d0e3094
HabibBeshara <habib@cyber-crafts.com>
2019-07-18 21:51:39 +0200

    removed console log from middleware

--- src/auth/middlewares/mainAuthMiddleware.ts
+++ src/auth/middlewares/mainAuthMiddleware.ts
@@ -2,7 +2,6 @@ import { getEndpointAuthConfig } from '../helpers'
 import App from '@cyber-crafts/ltc-core/lib/App'
 
 const mainAuthMiddleware = (app: App) => (req: any, res: any, next: any) => {
-  console.log('main auth started')
   const authConfig = getEndpointAuthConfig(app, req.url, req.method)
   if (!authConfig) {
     res.status(403)
